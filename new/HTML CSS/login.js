b216e527d9d4d0f69145b2dfa19b2129ab452900
riddhit3d <112148058+riddhit3d@users.noreply.github.com>
2022-10-03 13:29:04 +0530

    Add files via upload

---
+++ new/HTML CSS/login.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        var email = $('#email').val();
+        var password = $('#password').val();
+        $.getJSON("http://localhost:3000/users",
+            function (data) {
+                $.each(data, function (_key, value) {
+                    if (value.userEmail === email && value.userPassword === password && document.getElementById("email").value!=="" && document.getElementById("password").value!=="") {
+                        sessionStorage.setItem("email", email);
+                        $('form').submit();
+                    }
+                   
+                    else if(document.getElementById("email").value == ""){
+                        alert("Please Enter Email")
+                    }
+                    else if(document.getElementById("password").value == ""){
+                        alert("Please Enter password")
+                    }
+                    else {
+                        setTimeout(() => {
+                            $('#msg').html("<b>Wrong Crentials</b><br><br>");
+                        }, 1000)
+
+                    }
+                });
+            });
+    });
+});
+
